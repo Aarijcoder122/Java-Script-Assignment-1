4de84664142abc8b8a55daddb6b04183d1bafff9
Aarijcoder122 <aarijmaniar@gmail.com>
2025-10-18 21:27:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+// Chp1
+
+// question:2
+
+ alert("Error ! Please enter a valid password")
+
+// question:3
+
+alert("Welcome to JS land\nHappy coding!")
+
+// question:4
+
+alert("Welcome to JS land!")
+alert("Happy coding!\nPrevent this page from creating additional dialogs")
+
+// question:5
+
+console.log("hello i can run JS from my web browser console")
+
+// Chp 2
+
+// question:1
+
+var username;
+
+// question:2
+
+var myname = "Aarij Maniar"
+alert(myname)
+
+// question:3
+
+var message = "Hello World"
+alert(message)
+
+// question:4
+
+alert("Jhone Doe")
+alert("15 years old")
+alert("Certified Mobile Application Development")
+
+// question:5
+
+alert("Pizza\nPizz\nPiz\nPi\nP")
+
+// question:6
+
+var email = "My email-address is aarijmaniar@gamil.com"
+alert(email)
+
+// question:7
+
+var book = "I am trying to learn from the book A smarter4 way to learn javascript"
+alert(book)
+
+// question:8
+
+document.write("Yeah I can write HTML content through JavaScript")
+
+// question:9
+
+Ans : dont know;
+
+Chp 3
+
+// question:1
+
+var age = "I am 15 yaers old"
+alert(age)
+
+// question:2
+
+var visited = "You have visisted this site 14 times"
+alert(visited)
+
+// question:3
+
+var birthyear = "My birthyear is 1990 <br> Data type of my decalared variable is number "
+document.write(birthyear)
+
+// question:4
+
+document.write("<b>John Doe</b> ordered <b>5 T-shirt</b>(s) on XYZ clothing store ")
